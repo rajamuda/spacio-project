b5cfe9958a0f2bbb0d9bba5bae874e23ce3c0dcd
Fadhlal Khaliq Surado <fksutan.rajomudo@gmail.com>
2018-11-26 21:20:34 +0700

    Mempercantik submission

--- resources/js/pages/submission/add.vue
+++ resources/js/pages/submission/add.vue
@@ -1,11 +1,14 @@
 <template>
   <card>
-    <div class="m-auto p-2">
-      <div class="form-group row mx-auto">
+    <div class="m-auto p-4">
+      <div class="form-group row">
         <label class="col-md-3 col-form-label">Project's Name</label>
         <input type="text" v-model="projectName" name="projectName" class="col-md-8 form-control">
       </div>
       <div class="row">
+        <h5 class="col-md-12">Upload CSV Data</h5>
+      </div>
+      <div class="row m-0" style="background-color: #efefef">
         <div class="form-group col-md-6">
           <label class="col-md-12 col-form-label">SNPs Data</label>
           <input type="file" id="snps" class="col-md-12 form-control-file" accept="text/csv" @change="uploadFieldChange($event, 'snps')">
@@ -16,14 +19,44 @@
           <input type="file" id="phenotype" class="col-md-12 form-control-file" accept="text/csv" @change="uploadFieldChange($event, 'phenotype')">
           <span v-if="phenotype.length" class="col-md-12 text-muted">Size: {{ getFileSize('phenotype') }}</span>
         </div>
+        <div v-if="percentCompleted > 0" class="progress col-md-12 mb-2">
+          <div class="progress-bar progress-bar-striped" :class="{'bg-success': (percentCompleted == 100), 'bg-danger': (errors != '')}" role="progressbar" :style="{width: percentCompleted + '%'}" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"> {{ percentCompleted }}% </div>
+        </div>   
       </div>
-      <div class="form-group py-2 col-md-12">
-        <button class="btn btn-success" @click="submit">Upload</button>
-      </div>
+      <!-- Parameter settings -->
+      <div id="accordion">
 
-      <div v-if="percentCompleted > 0" class="progress">
-        <div class="progress-bar progress-bar-striped" :class="{'bg-success': (percentCompleted == 100), 'bg-danger': (errors != '')}" role="progressbar" :style="{width: percentCompleted + '%'}" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"> {{ percentCompleted }}% </div>
+          <div id="headingOne">
+            <h5 class="mb-0">
+              <button class="p-0 btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                <fa icon="caret-square-down" fixed-width/> Advanced settings
+              </button>
+            </h5>
+          </div>
+        <div class="px-2">
+          <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+            <div class="row mx-auto">
+              <div class="col-md-6">Minor Allele Frequency (MAF)</div>
+              <div class="col-md-2"><input type="text" class="form-control form-control-sm" name="maf" value="0.01"/></div>
+            </div> 
+            <div class="row mx-auto">
+              <div class="col-md-6">Minimum SNPs to process</div>
+              <div class="col-md-2"><input type="text" class="form-control form-control-sm" name="maf" value="100"/></div>
+            </div>      
+            <div class="row mx-auto">
+              <div class="col-md-6">Remove Monomorphism</div>
+              <div class="col-md-2 text-right"><input type="checkbox" class="form-check-input" name="monomorphism" checked/></div>
+            </div>
+            <div class="row mx-auto">
+              <div class="col-md-6">Only use highly ranked SNPs</div>
+              <div class="col-md-2 text-right"><input type="checkbox" class="form-check-input" name="ranked" checked/></div>
+            </div>                       
+          </div>
+        </div>
       </div>
+      <div class="form-group pt-4 px-0 col-md-12">
+        <button class="btn btn-success" @click="submit">Upload</button>
+      </div>   
     </div>
   </card>
 </template>
